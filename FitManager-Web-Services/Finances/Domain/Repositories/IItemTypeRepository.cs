7fa70d6df9cec63a894c57b0639f47e0fec08cb7
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2025-06-14 00:53:06 -0500

    feat(finances): add domain repositories (IItemTypeRepository and IPurchaseDetailRepository)

---
+++ FitManager-Web-Services/Finances/Domain/Repositories/IItemTypeRepository.cs
@@ -0,0 +1,8 @@
+using FitManager_Web_Services.Inventory.Domain.Model.Aggregates;
+using FitManager_Web_Services.Shared.Domain.Repositories;
+
+namespace FitManager_Web_Services.Inventory.Domain.Repositories;
+
+public interface IItemTypeRepository : IBaseRepository<ItemType>
+{
+}
