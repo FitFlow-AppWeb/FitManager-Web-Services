e21e48020c9eb6dba9270987b32a5e709b332014
victortiz21 <128175967+Nico1234556@users.noreply.github.com>
2025-06-14 16:55:35 -0500

    changes

--- FitManager-Web-Services/obj/Debug/net9.0/FitManager-Web-Services.AssemblyInfo.cs
+++ FitManager-Web-Services/obj/Debug/net9.0/FitManager-Web-Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitManager-Web-Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18f1ad33be4914f410896a3b25b9be4d5308eb06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63d25c37c0886bba63af7c292a2e699f05416d91")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitManager-Web-Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitManager-Web-Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
