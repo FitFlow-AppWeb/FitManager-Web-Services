8a4fd7ccc4015ee2388dd262cc23a039c0efda46
Tomio Nakamurakare <tomio.nakamura15@gmail.com>
2025-06-20 01:15:59 -0500

    Improve Classes GET response

--- FitManager-Web-Services/Classes/Interfaces/REST/Transform/ClassResourceFromEntityAssembler.cs
+++ FitManager-Web-Services/Classes/Interfaces/REST/Transform/ClassResourceFromEntityAssembler.cs
@@ -1,5 +1,8 @@
 using FitManager_Web_Services.Classes.Domain.Model.Aggregates;
 using FitManager_Web_Services.Classes.Interfaces.REST.Resources;
+using FitManager_Web_Services.Members.Interfaces.REST.Transform;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace FitManager_Web_Services.Classes.Interfaces.REST.Transform;
 
@@ -7,15 +10,25 @@ public static class ClassResourceFromEntityAssembler
 {
     public static ClassResource ToResource(Class entity)
     {
+        IEnumerable<MemberResource>? enrolledMembers = null;
+        
+        if (entity.ClassMembers != null && entity.ClassMembers.Any())
+        {
+            enrolledMembers = entity.ClassMembers
+                .Select(cm => MemberResourceFromEntityAssembler.ToResourceFromEntity(cm.Member));
+        }
+
         return new ClassResource(
             entity.Id,
             entity.Name,
             entity.Description,
-            entity.Type,
+            entity.Type,       
             entity.Capacity,
             entity.StartDate,
             entity.Duration,
             entity.Status,
-            entity.EmployeeId);
+            entity.EmployeeId,
+            enrolledMembers
+        );
     }
 }
