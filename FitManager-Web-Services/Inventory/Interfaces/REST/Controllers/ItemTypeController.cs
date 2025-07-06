017554612455b05b9723e59ca111d38b12affb78
kcc12321 <cassiusmartel21042006@gmail.com>
2025-07-06 17:19:05 -0500

    Add new backend translations for item type

--- FitManager-Web-Services/Inventory/Interfaces/REST/Controllers/ItemTypeController.cs
+++ FitManager-Web-Services/Inventory/Interfaces/REST/Controllers/ItemTypeController.cs
@@ -1,12 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using FitManager_Web_Services.Inventory.Application.Internal.CommandServices;
 using FitManager_Web_Services.Inventory.Application.Internal.QueryServices;
-using FitManager_Web_Services.Inventory.Domain.Model.Commands;
-using FitManager_Web_Services.Inventory.Domain.Model.Queries; // This using is correct
+using FitManager_Web_Services.Inventory.Domain.Model.Queries;
 using FitManager_Web_Services.Inventory.Interfaces.REST.Resources;
 using FitManager_Web_Services.Inventory.Interfaces.REST.Transform;
 using Swashbuckle.AspNetCore.Annotations;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Localization; 
+using FitManager_Web_Services.Resources; 
 
 namespace FitManager_Web_Services.Inventory.Interfaces.REST.Controllers
 {
@@ -16,21 +16,38 @@ namespace FitManager_Web_Services.Inventory.Interfaces.REST.Controllers
     {
         private readonly ItemTypeCommandService _itemTypeCommandService;
         private readonly ItemTypeQueryService _itemTypeQueryService;
+        private readonly IStringLocalizer<SharedResource> _localizer; 
 
-        public ItemTypesController(ItemTypeCommandService itemTypeCommandService, ItemTypeQueryService itemTypeQueryService)
+        public ItemTypesController(
+            ItemTypeCommandService itemTypeCommandService,
+            ItemTypeQueryService itemTypeQueryService,
+            IStringLocalizer<SharedResource> localizer) 
         {
             _itemTypeCommandService = itemTypeCommandService;
             _itemTypeQueryService = itemTypeQueryService;
+            _localizer = localizer; 
         }
 
         [HttpGet]
         [SwaggerOperation(Summary = "Listar Tipos de Ítem", Description = "Obtiene una lista de todos los tipos de ítem registrados.")]
-        public async Task<ActionResult<IEnumerable<ItemTypeResource>>> GetAllItemTypes()
+        public async Task<ActionResult<object>> GetAllItemTypes()  
         {
-            var getAllQuery = new GetAllItemTypesQuery(); // <-- Corrected here
+            var getAllQuery = new GetAllItemTypesQuery();
             var itemTypes = await _itemTypeQueryService.Handle(getAllQuery);
             var itemTypeResources = ItemTypeResourceFromEntityAssembler.ToResourceListFromEntityList(itemTypes);
-            return Ok(itemTypeResources);
+
+            var localizedMessage = _localizer["ItemTypesRetrievedSuccessfully"];
+            return Ok(new
+            {
+                message = new
+                {
+                    name = "ItemTypesRetrievedSuccessfully",
+                    value = localizedMessage.Value,
+                    resourceNotFound = localizedMessage.ResourceNotFound,
+                    searchedLocation = localizedMessage.SearchedLocation
+                },
+                data = itemTypeResources
+            });
         }
 
         [HttpPost]
@@ -38,19 +55,53 @@ namespace FitManager_Web_Services.Inventory.Interfaces.REST.Controllers
         public async Task<IActionResult> CreateItemType([FromBody] CreateItemTypeResource resource)
         {
             if (!ModelState.IsValid)
-                return BadRequest(ModelState);
+            {
+                var invalidMessage = _localizer["InvalidItemTypeData"]; 
+                return BadRequest(new
+                {
+                    message = new
+                    {
+                        name = "InvalidItemTypeData",
+                        value = invalidMessage.Value,
+                        resourceNotFound = invalidMessage.ResourceNotFound,
+                        searchedLocation = invalidMessage.SearchedLocation
+                    },
+                    errors = ModelState 
+                });
+            }
 
             var createCommand = CreateItemTypeCommandFromResourceAssembler.ToCommandFromResource(resource);
-            
-            var createdItemType = await _itemTypeCommandService.Handle(createCommand); 
+
+            var createdItemType = await _itemTypeCommandService.Handle(createCommand);
 
             if (createdItemType == null)
             {
-                return BadRequest("Failed to create item type. It might already exist or there was another business rule violation.");
+                var failedMessage = _localizer["FailedToCreateItemType"]; 
+                return BadRequest(new
+                {
+                    message = new
+                    {
+                        name = "FailedToCreateItemType",
+                        value = failedMessage.Value,
+                        resourceNotFound = failedMessage.ResourceNotFound,
+                        searchedLocation = failedMessage.SearchedLocation
+                    }
+                });
             }
 
             var itemTypeResource = ItemTypeResourceFromEntityAssembler.ToResourceFromEntity(createdItemType);
-            return CreatedAtAction(nameof(GetAllItemTypes), new { id = itemTypeResource?.Id }, itemTypeResource);
+            var successMessage = _localizer["ItemTypeCreatedSuccessfully"]; 
+            return CreatedAtAction(nameof(GetAllItemTypes), new { id = itemTypeResource?.Id }, new
+            {
+                message = new
+                {
+                    name = "ItemTypeCreatedSuccessfully",
+                    value = successMessage.Value,
+                    resourceNotFound = successMessage.ResourceNotFound,
+                    searchedLocation = successMessage.SearchedLocation
+                },
+                data = itemTypeResource
+            });
         }
     }
 }
